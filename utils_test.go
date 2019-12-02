40893e8cb36b144e759d817149899386a47d5b17
xuyang <y.xu@neoclub.cn>
2019-12-02 12:08:08 +0800

    fix:上传新版本

--- utils_test.go
+++ utils_test.go
@@ -9,3 +9,27 @@ func TestSortIdToLongId(t *testing.T) {
 		t.Log("转换通过")
 	}
 }
+
+func TestLongIdToSortId(t *testing.T) {
+	if id, err := LongIdToSortId("000000000000000000000001"); err != nil || id != 1 {
+		t.Error("转换错误")
+	} else {
+		t.Log("转换通过")
+	}
+}
+
+func TestStringToObjectId(t *testing.T) {
+	if id, err := StringToObjectId("1"); err != nil || id.Hex() != "000000000000000000000001" {
+		t.Error("转换错误")
+	} else {
+		t.Log("success")
+	}
+}
+
+func TestStringToObjectIdError(t *testing.T) {
+	if id, err := StringToObjectId("abc"); err != nil && id == nil {
+		t.Error("success")
+	} else {
+		t.Log("err")
+	}
+}
