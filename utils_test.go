5d8662b6220368bef048526806f9ca3d4c313aca
xuyang <y.xu@neoclub.cn>
2019-12-02 12:27:39 +0800

    fix:上传新版本

--- utils_test.go
+++ utils_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -33,5 +34,32 @@ func TestStringToObjectIdError(t *testing.T) {
 		t.Error("success")
 	} else {
 		t.Log("err")
+	}
+}
+
+type Demo struct {
+	A uint8   `default:"1"`
+	B uint16  `default:"2"`
+	C uint32  `default:"3"`
+	D uint    `default:"4"`
+	E uint64  `default:"5"`
+	F int8    `default:"6"`
+	G int16   `default:"7"`
+	H int32   `default:"8"`
+	I int     `default:"9"`
+	J int64   `default:"10"`
+	K float32 `default:"0.1"`
+	L float64 `default:"0.2"`
+	M string  `default:"str"`
+	N bool    `default:"true"`
+}
+
+func TestDefault(t *testing.T) {
+	d := new(Demo)
+	if err := Default(d); err == nil {
+		t.Log("success")
+	} else {
+		fmt.Println(err)
+		t.Error("err")
 	}
 }
