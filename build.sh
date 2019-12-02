5ce1b64e7201f1e8bd748e024148bddacf157029
xuyang <y.xu@neoclub.cn>
2019-12-02 11:44:26 +0800

    fix:上传新版本

--- build.sh
+++ build.sh
@@ -9,14 +9,20 @@ go tool cover -html=./test/testReport.out -o=./test/testReport.html
 VERSION=$1
 echo $VERSION
 if [ "$VERSION" ];then
+    echo "tag版本号为$VERSION"
+else
     echo "请输入tab版本号";
     exit 1;
-else
-    echo "tag版本号为$VERSION"
 fi;
 
 git add .
 git commit -m "fix:上传新版本"
 git tag $VERSION
+if [ $? -eq 0 ]; then
+    echo "tag成功";
+else
+    echo "打tag失败。";
+    exit 1
+fi;
 git push origin master
 git push origin $VERSION
